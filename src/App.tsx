8f6b959da8d80e3696322220ef720bdc4202e270
BaconLover307 <kresnadi30@gmail.com>
2024-04-14 16:43:52 +0700

    Setup base for emergency design 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
-    const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const [isFlip, setIsFlip] = useState(false);
 
-    return (
-      <main className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+  const flip = () => {
+    setIsFlip(!isFlip);
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('scroll', flip);
+
+    return () => {
+      document.body.removeEventListener('scroll', flip);
+    };
+  });
+
+  return (
+    <main className="App">
+      <div id="flip-card" onClick={flip} className="flip-card">
+        <div className={`flip-card-inner ${isFlip ? 'flip' : ''}`}>
+          <div className="flip-card-front">
+            <div id="wolven-dev" className="main-card">
+              <h3>WOLVEN.DEV</h3>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <div className="main-card">
+              <p>My name is Jovan Kresnadi</p>
+            </div>
+          </div>
         </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button className="bg-blue-500" onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      </main>
-    );
-}
+      </div>
+    </main>
+  );
+};
 
 export default App;
