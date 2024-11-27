0a7cee2e1f0e449616cb0528e2af66941e1ec9e1
BaconLover307 <kresnadi30@gmail.com>
2024-11-27 17:14:26 +0700

    Finalize card flip animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,149 +1,88 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
 import $ from 'jquery';
 import './App.css';
 
-const X_SPIN_DEG = 10;
-const Y_SPIN_DEG = 25;
-const CARD_DEADZONE_X = 0.4;
-const CARD_DEADZONE_Y = 0.5;
-
 const App: React.FC = () => {
-  const [isFlip, setIsFlip] = useState(false);
+  const [flipAmount, setFlipAmount] = useState(0);
+  const [cardDirection, setCardDirection] = useState(0);
 
-  const pages = [
-    {
-      content: `
-        <div id="wolven-dev" className="main-card">
-          <h3>WOLVEN.DEV</h3>
-        </div>
-        `,
-    },
-    {
-      content: `
-        <div id="wolven-dev" className="main-card">
-          <h3>WOLVEN.DEV</h3>
-        </div>
-        `,
-    },
-    {
-      content: `
-        <div className="main-card">
-          <p>My name is Jovan Kresnadi</p>
-        </div>
-        `,
-    },
-    {
-      content: `
-        <div className="main-card">
-          <p>Welcome to my page :)</p>
-        </div>
-        `,
-    },
-  ];
+  const cardFaces = [
+    `
+      <div id="wolven-dev" class="main-card">
+        <h3>WOLVEN.DEV</h3>
+      </div>
+    `,
+    `
+      <div class="main-card">
+        <p>My name is Jovan Kresnadi</p>
+      </div>
+    `,
+    `
+      <div class="main-card">
+        <h3>Welcome!</h3>
+      </div>
+    `,
+  ]
+
 
   const flip = () => {
-    setIsFlip(!isFlip);
+    const newFlip = (flipAmount - 180);
+    $('.flip-card-inner').css('--rotation', `${newFlip}deg`);
+    $('.flip-card-inner').css('transform', `rotateX(--rotation)`);
+    
+    const changedCardFace = Math.abs(newFlip)%360 == 180 ? $("#CardBack") : $("#CardFront");
+    const newCardFace = (cardDirection + 1)%cardFaces.length;
+    changedCardFace.html(cardFaces[newCardFace]);
+    setCardDirection(newCardFace);
+    
+    $('.flip-card').css('pointer-events', `none`);
+    setTimeout(() => {
+      
+      if (newFlip <= -720) {
+        $('.flip-card-inner').css('transition', `none`);
+        $('.flip-card-inner').css('--rotation', `0deg`);
+        setTimeout(() => {
+          $('.flip-card').css('pointer-events', `auto`);
+          $('.flip-card-inner').css('transition', `transform 500ms`);
+          setFlipAmount(0)
+        }, 50)
+      }
+        else {
+          $('.flip-card').css('pointer-events', `auto`);
+          setFlipAmount(newFlip);
+        }
+    }, 500)
   };
-
-  function clamp(num: number, min: number, max: number): number {
-    return num <= min ? min : num >= max ? max : num;
-  }
 
   const handleClick = (e: React.MouseEvent) => {
-    console.log(e.pageX);
-    console.log(e.pageY);
     flip();
-    animateFlip(e.pageX, e.pageY);
-  };
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    animateCard(e.pageX, e.pageY);
-  };
-
-  const calculateRotation = (mouseX: number, mouseY: number, isFlip: boolean) => {
-    const cardPosition = $('#flip-card').position();
-    const posX = mouseX - Math.floor(cardPosition.left);
-    const posY = mouseY - Math.floor(cardPosition.top);
-    const cardHeight = $('#flip-card').outerHeight() ?? 0;
-    const halfCardHeight = cardHeight / 2;
-    const cardWidth = $('#flip-card').outerWidth() ?? 0;
-    const halfCardWidth = cardWidth / 2;
-
-    const x = posX - cardWidth / 2;
-    const y = cardHeight / 2 - posY;
-
-    const cardZoneStartX = halfCardWidth * CARD_DEADZONE_X;
-    const cardZoneStartY = halfCardHeight * CARD_DEADZONE_Y;
-
-    const cardZoneX = halfCardWidth * CARD_DEADZONE_X;
-    const cardZoneY = halfCardHeight * CARD_DEADZONE_Y;
-
-    const xClamp = clamp(Math.abs(x), cardZoneStartX, halfCardWidth);
-    const yClamp = clamp(Math.abs(y), cardZoneStartY, halfCardHeight);
-
-    const isFlipped = isFlip ? 1 : -1;
-    const xMult = ((((xClamp - cardZoneStartX) / cardZoneX) * x) / Math.abs(x)) * -1;
-    const yMult = ((((yClamp - cardZoneStartY) / cardZoneY) * y) / Math.abs(y)) * isFlipped;
-    const direction = isFlip ? -180 : 0;
-    return { rotateY: xMult * X_SPIN_DEG + direction, rotateX: yMult * Y_SPIN_DEG };
-  };
-
-  const animateCard = (mouseX: number, mouseY: number) => {
-    if (isFlip) return;
-    const { rotateY, rotateX } = calculateRotation(mouseX, mouseY, isFlip);
-
-    $('.flip-card-inner').css('transition', `transform ${rotateY > 0 && rotateX > 0 ? '200' : '800'}ms`);
-    $('.flip-card-inner').css('transform', `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`);
-  };
-
-  const animateFlip = (mouseX: number, mouseY: number) => {
-    const { rotateY, rotateX } = calculateRotation(mouseX, mouseY, !isFlip);
-
-    $('.flip-card-inner').css('transition', `transform 800ms`);
-    $('.flip-card-inner').css('transform', `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`);
-  };
-
-  const handleMouseLeave = () => {
-    $('.flip-card-inner').css('transition', `transform 800ms`);
-    $('.flip-card-inner').css('transform', `rotateY(${isFlip ? -180 : 0}deg) rotateX(0deg)`);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>): void => {
-    // if (e.key == 'Enter') {
-    //   const cardPosition = $('#flip-card').position();
-    //   const cardHeight = $('#flip-card').outerHeight() ?? 0;
-    //   const halfCardHeight = cardHeight / 2;
-    //   const cardWidth = $('#flip-card').outerWidth() ?? 0;
-    //   const halfCardWidth = cardWidth / 2;
-    //   animateFlip(cardPosition.left + halfCardWidth, cardPosition.top + halfCardHeight);
-    //   flip();
-    // }
+      return
   };
 
   return (
     <main className="App">
-      {/* <div id="flip-card" onClick={flip} className="flip-card"> */}
       <div
         id="flip-card"
         tabIndex={0}
         role="button"
         onClick={handleClick}
         onKeyDown={handleKeyDown}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
         className="flip-card"
       >
-        <div className={`flip-card-inner ${isFlip ? 'flip' : ''}`}>
-          <div className="flip-card-front" id="CardFront">
-            <div id="wolven-dev" className="main-card">
-              <h3>WOLVEN.DEV</h3>
+          <div className={`flip-card-inner`}>
+            <div className="flip-card-front" id="CardFront">
+              <div id="wolven-dev" className="main-card">
+                <h3>WOLVEN.DEV</h3>
+              </div>
             </div>
-          </div>
-          <div className="flip-card-back" id="CardBack">
-            <div className="main-card">
-              <p>My name is Jovan Kresnadi</p>
-            </div>
+            <div className="flip-card-back" id="CardBack">
+              <div className="main-card">
+                <p>My name is Jovan Kresnadi</p>
+              </div>
           </div>
         </div>
       </div>
