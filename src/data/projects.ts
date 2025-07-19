6e426b9251944dbb01a25332ad9c0005332e39a1
Jovan Kresnadi <jovan.kresnadi@wolven.dev>
2025-07-19 09:19:23 +0700

    Update Resume July 2025, update style on Experience & Project section (#12)

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,7 +16,7 @@ const projects: DProject[] = [
     iconFile: 'GradleVisualizer.svg',
     url: 'https://tree-self.vercel.app/',
     description:
-      'Gradle Visualizer is a web tool to visualize complex Gradle dependencies into a tree-like interactive 3D graph. It allows users to easily navigate and understand the dependency structure and identify potential issues. In this freelance project, I helped refactor the codebase and add additional functionalities. It uses React Three Fiber, a React renderer for Three.js, integrated into a Next.js app.',
+      'Gradle Visualizer is a web tool to **visualize complex Gradle dependencies** into a **interactive 3D graph**. It allows users to easily navigate and understand the dependency structure and **identify potential conflicts & issues.** In this freelance project, I helped refactor the codebase and add additional functionalities. Built **React Three Fiber**, a React renderer for Three.js, integrated into a **Next.js** app.',
     date: '2025',
     techs: ['Next.js', 'Three.js', 'Typescript', 'TailwindCSS'],
   },
@@ -27,7 +27,7 @@ const projects: DProject[] = [
     file: '',
     url: 'https://porsee-website-demo.vercel.app/',
     description:
-      'Porsee is an Indonesian food catering service delivering fresh ready-to-cook ingredients to your doorstep. Porsee aims to bring restaurant quality food to your home kitchen, complete with the experience of cooking it yourself. I developed the website using Next.js and Firebase as the Backend, deployed to a Digital Ocean droplet with Docker to support containerization.',
+      'Porsee is an **Indonesian online food catering service** delivering fresh ready-to-cook ingredients to your doorstep. Porsee aims to bring restaurant quality food to your home kitchen, complete with the experience of cooking it yourself. I developed the website using **Next.js**, developed back-end services using **Firebase** with integrated **Google Analytics**. Deployed to a **Digital Ocean** droplet with **Docker.**',
     date: '2023',
     techs: ['Next.js', 'Typescript', 'TailwindCSS', 'Figma', 'Firebase', 'Digital Ocean', 'Docker'],
   },
@@ -38,7 +38,7 @@ const projects: DProject[] = [
     file: '',
     url: 'https://wolven.dev/',
     description:
-      "It's basically this website... \n I used Vite React with Typescript, and Tailwind to style and the occasional vanilla CSS to help out with complex animations.",
+      "It's basically this website... I used **Vite React with Typescript**, and **Tailwind** to style and the occasional vanilla CSS to help out with complex animations. Deployed to **GitHub Pages.**",
     date: '2023',
     techs: ['Vite', 'React', 'Typescript', 'TailwindCSS', 'Figma'],
   },
