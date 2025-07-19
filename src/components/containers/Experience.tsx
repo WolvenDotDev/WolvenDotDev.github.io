69d843ed4553cbfc7a2c0dd7bd83f512ee3a504a
WolvenDotDev <jovan.kresnadi@wolven.dev>
2025-07-19 08:25:32 +0700

    Update Experience section style and content

--- src/components/containers/Experience.tsx
+++ src/components/containers/Experience.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { DExperience } from '@/data/experience';
+import { micromark } from 'micromark';
 import './container.css';
 
 type ExpProps = {
@@ -7,22 +8,30 @@ type ExpProps = {
 };
 
 const Experience: React.FunctionComponent<ExpProps> = ({ experience }) => {
-  const { id, title, company, description, jobdesc, date, skills } = experience;
+  const { id, title, company, description, jobdesc, date, skills, companyUrl } = experience;
   return (
     <div className="exp-card flex flex-col lg:gap-4 gap-6 transition-colors duration-200 hover:opacity-100 hover:bg-light-1 hover:bg-opacity-5 rounded-lg p-4">
-      <div className="flex lg:flex-row flex-col-reverse justify-between items-baseline gap-x-2">
+      <div className="flex lg:flex-row flex-col-reverse justify-between items-baseline gap-x-2 gap-y-1">
         <h6 className="text-light-1 lg:w-1/5">{date}</h6>
-        <h4 className="lg:w-[78%]">
-          {title} · {company}
-        </h4>
+        <div className="lg:w-[78%] flex flex-col items-start gap-x-2">
+          <h4 className="">{title}</h4>
+          <a href={companyUrl} target="_blank" rel="noreferrer" className="cursor-pointer flex company-link">
+            <h5>{company}</h5>
+            {companyUrl && (
+              <span>
+                <i className="link-icon fa fa-link"></i>
+              </span>
+            )}
+          </a>
+        </div>
       </div>
       <div className="flex justify-end">
         <div className="lg:w-[78%] flex flex-col gap-4">
-          {description && <p>{description}</p>}
+          {description && <p dangerouslySetInnerHTML={{ __html: micromark(description) }} />}
 
           <ul className="text-neutral-1 list-disc text-justify pl-2 flex flex-col gap-2 jobdesc-list">
             {jobdesc.map((job, idx) => (
-              <li key={`${id}-job-${idx}}`}>{job}</li>
+              <li key={`${id}-job-${idx}}`} dangerouslySetInnerHTML={{ __html: micromark(job) }} />
             ))}
           </ul>
           <ul className="flex flex-wrap gap-3">
--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,8 +2,9 @@ export type DExperience = {
   id: string;
   title: string;
   company: string;
+  companyUrl?: string;
   description?: string;
-  jobdesc: string[];
+  jobdesc: string[]; // Markdown
   date: string;
   skills?: string[];
 };
@@ -13,27 +14,45 @@ const Experiences: DExperience[] = [
     id: 'solecode',
     title: 'Software Engineer',
     company: 'Solecode',
-    description: 'Solecode is an IT consulting and services company specializing in rapid delivery, intuitive design, convenient experience, high agility, and better client collaboration.',
+    companyUrl: 'https://solecode.id',
+    description:
+      'Solecode is an IT consulting and services company specializing in rapid delivery, intuitive design, convenient experience, high agility, and better client collaboration.',
     jobdesc: [
-      'Refactored a Human Resource management app using Dart & Flutter, refining UI logic and rebuilding the overall project structure',
-      'Developed back-end services and web pages for a budget monitoring system using Blazor .NET Web Framework, Tailwind CSS, and PostgreSQL DBMS',
-      'Developed a UI component library in React TypeScript with Vite, utilizing Tailwind CSS, and Storybook as a front-end workshop for UI development, and publishing it as an npm package for future projects',
-      'Built UI components and back-end services for a resource management website of an Indonesian oil and gas company using C# with ASP.NET MVC framework and Razor Pages',
-      'Maintained a Next.js code boilerplate for streamlined development in future projects',
-      'Developed back-end services for a thesis management website and a student study plan platform for a world-class private university in Jakarta, using C# with ASP.NET Core framework, Azure CosmosDB as NoSQL database, and Azure cloud services',
+      'Developing a **document storage management system** using **Java** with **Spring & ZK** framework, **streamlining business workflows** across all branches of an Indonesian financing company',
+      'Led a team of 3, **restructuring** a Human Resource management app using **Flutter**, implementing **state management** with Riverpod, **caching**, and building reusable components, resulting in a **reduction of over 10,000 lines of code**',
+      'Unified **React TypeScript** UI components into a **Storybook** UI library with **Vite, Tailwind,** and published it as an internal **npm package** to **reduce development time by 50%**',
+      'Developed **core back-end logic** for an **oil reservoir and well management system** of an Indonesian oil and gas company using .NET MVC, **reducing development time** by **30%**',
+      'Maintained a **Next.js fullstack boilerplate** to **bootstrap** future company **projects**',
+      'Developed **back-end services** for a thesis management and **student study plan platform** for a private university in Jakarta, using **C#** with **ASP.NET Core, Redis, Azure CosmosDB,** and **Azure cloud services**',
     ],
     date: 'Feb 2023 - Present',
-    skills: ['React', 'Next.js', 'TailwindCSS', 'C#', '.NET', 'Azure', 'OracleDB', 'Storybook', 'Flutter', 'Figma'],
+    skills: [
+      'React',
+      'Next.js',
+      'TailwindCSS',
+      'C#',
+      '.NET',
+      'Java',
+      'Azure',
+      'OracleDB',
+      'Redis',
+      'PostgreSQL',
+      'Storybook',
+      'Flutter',
+      'Figma',
+    ],
   },
   {
     id: 'dana',
     title: 'Front End SDET Intern',
     company: 'DANA',
-    description: 'DANA Indonesia is a payment gateway company aiming to replace traditional wallets with their digital platform.',
+    companyUrl: 'https://dana.id',
+    description:
+      'DANA Indonesia is a payment gateway company aiming to replace traditional wallets with their digital platform.',
     jobdesc: [
-      'Ensured high-quality user experiences for lifestyle services and mini-program features through automated testing on iOS devices using Selenium and XCode, increasing test coverage to 95%',
-      'Developed test suites for end-to-end, regression, and system integration tests using XMind and Testrail',
-      'Assisted in on-duty monitoring during high-traffic events and post-deployments, reporting issues in JIRA with severity and priority classification',
+      'Ensured high-quality user experiences for lifestyle services and mini-program features through **automated testing** on iOS devices using Selenium and Xcode, **increasing test coverage to 95%**',
+      '**Developed test suites** for **end-to-end, regression,** and **system integration tests** using XMind and Testrail',
+      'Assisted in monitoring during high-traffic events and post-deployments, reporting issues in Jira with severity and priority classification',
     ],
     date: 'Aug 2021 - Feb 2022',
     skills: ['Cucumber', 'Java', 'Selenium'],
@@ -42,10 +61,12 @@ const Experiences: DExperience[] = [
     id: 'blibli',
     title: 'SDET Intern',
     company: 'Blibli.com',
+    companyUrl: 'https://www.blibli.com',
     description: 'Blibli.com is an omnichannel e-commerce platform and lifestyle ecosystem in Indonesia.',
     jobdesc: [
-      'Increased regression test coverage to 95% for checkout and promotion features by developing test cases using Behavior Driven Development and creating automated tests for API & UI testing using Cucumber & Selenium',
-      'Maintained sanity tests and integration tests at 100% coverage.',
+      '**Increased** regression **test coverage** to **95%** for checkout and promotion features',
+      'Developed **test cases** using **Behavior Driven Development (BDD)** to enhance developer productivity',
+      'Created **automated tests** for **API & UI testing** using Cucumber & Selenium',
     ],
     date: 'Jun - Aug 2021',
     skills: ['Cucumber', 'Selenium', 'Java', 'Kafka'],
@@ -57,7 +78,7 @@ const Experiences: DExperience[] = [
     description:
       'Course management in ITB is divided into several laboratories. Programming Lab manages Data Structure & Algorithm, Object Oriented Programming, and Web Development.',
     jobdesc: [
-      'Assisted and supervised students in coding lessons and practice using Olympia learning platform',
+      'Assisted and supervised students in coding assignments and practice using Olympia learning platform',
       'Managed the development of project assignments for the Data Structures & Algorithm course',
       'Designed and graded project assignments for the Object Oriented Programming course',
     ],
@@ -68,7 +89,8 @@ const Experiences: DExperience[] = [
     id: 'itb',
     title: 'Bachelor of Science in Informatics',
     company: 'Institut Teknologi Bandung',
-    jobdesc: ['GPA: 3.62/4.00', 'Vice Head of Publications Department at Informatics Student Union (HMIF ITB)'],
+    companyUrl: 'https://www.itb.ac.id',
+    jobdesc: ['*Cum Laude* (GPA 3.62)', 'Vice Head of Publications Department at Informatics Student Union (HMIF ITB)'],
     date: '2018 - 2022',
   },
 ];
